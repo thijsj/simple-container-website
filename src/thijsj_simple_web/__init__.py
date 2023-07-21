197a5ee0a17bb753fc8ab5cead1e49a601cf47aa
Thijs Janssen <thijs@seecr.nl>
2023-07-21 12:34:45 +0200

    TJ: simple web app

---
+++ src/thijsj_simple_web/__init__.py
@@ -0,0 +1,31 @@
+## begin license ##
+#
+# Learn creating a simple website and hosting it using containers.
+#
+# Copyright (C) 2023 Thijs Janssen https://thijsj.nl
+#
+# This file is part of "Simple Container Website"
+#
+# "Simple Container Website" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Simple Container Website" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Simple Container Website".  If not, see <http://www.gnu.org/licenses/>.
+#
+## end license ##
+
+from quart import Quart
+
+app = Quart(__name__)
+
+@app.route('/')
+async def hello():
+    return 'Hoi'
+
